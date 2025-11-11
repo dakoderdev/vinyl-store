aaa5351a2eff0cce981cb247eafafe0ffdb87e09
dakodev <123116563+dakoderdev@users.noreply.github.com>
2025-11-10 23:19:53 -0300

    agregar pagina de detalles para los vinilos

---
+++ ProyectosWeb/vinyl-store/js/data.js
@@ -0,0 +1,11 @@
+const productos = [
+  { nombre: "Brat", precio: 16500, img: "vinyl-brat.jpg", artista: "Charli XCX", descripcion: "Edición limitada en vinilo negro de 180g. Incluye 15 tracks explosivos." },
+  { nombre: "The Life of a Showgirl", precio: 18000, img: "vinyl-showgirl.jpg", artista: "Kylie Minogue", descripcion: "Colección de grandes éxitos de la Reina del Pop. Doble vinilo de color rosa." },
+  { nombre: "Short & Sweet Deluxe", precio: 20000, img: "vinyl-shortsweetdeluxe.jpg", artista: "Sabrina Carpenter", descripcion: "Versión deluxe del aclamado álbum. Incluye tracks extra y póster exclusivo." },
+  { nombre: "AM", precio: 20100, img: "vinyl-am.jpg", artista: "Arctic Monkeys", descripcion: "El quinto álbum de estudio de la banda. Vinilo negro clásico con arte icónico." },
+  { nombre: "Future Nostalgia", precio: 17350, img: "vinyl-futurenostalgia.jpg", artista: "Dua Lipa", descripcion: "Disco de pop retro ganador de múltiples premios. Vinilo azul translúcido." },
+  { nombre: "Norman F*cking Rockwell", precio: 16000, img: "vinyl-nfr.jpg", artista: "Lana del Rey", descripcion: "Una obra maestra aclamada por la crítica. Doble LP en color azul pálido." },
+  { nombre: "Come Over When You're Sober", precio: 16000, img: "vinyl-comesober.jpg", artista: "Lil Peep", descripcion: "Álbum póstumo que define una generación. Vinilo con efecto splatter." },
+  { nombre: "1989", precio: 17000, img: "vinyl-1989.jpg", artista: "Taylor Swift", descripcion: "Taylor's Version. Edición Crystal Skies Blue de la reedición." },
+  { nombre: "Starboy", precio: 18250, img: "vinyl-starboy.jpg", artista: "The Weeknd", descripcion: "Doble vinilo rojo de 140g. Incluye colaboraciones estelares." },
+];
